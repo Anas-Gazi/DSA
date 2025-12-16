84f8de5b4b116d6138dec3531d18c3ef43e0426c
Anas <gazianas2020@gmail.com>
2025-12-16 12:53:09 +0600

    Add files via upload

---
+++ DSA in C++/C++ String/Without function/binaryStringToBinaryNum2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string addBinary(string a, string b){
+  string result;
+  int i = a.size() -1;
+  int j = b.size()-1;
+  int carry = 0;
+
+  while(i>=0 || j>=0 || carry){
+    int sum = carry;
+
+    if(i>=0) sum+= a[i--] -'0';
+    if(j>=0) sum+= b[j--] - '0';
+    result.push_back((sum % 2) + '0');
+    carry= sum/2;
+  
+  }
+  reverse(result.begin(), result.end());
+// Remove leading zeros
+int pos = 0;
+while(pos < result.size() - 1 && result[pos] == '0') {
+pos++;
+}
+  return result;
+}
+int main(){
+  string a = "1011";
+string b = "01010";
+
+cout << addBinary(a, b);  // Output: 10001
+
+}
+
+
